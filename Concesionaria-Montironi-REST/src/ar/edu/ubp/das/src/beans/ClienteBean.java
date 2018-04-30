28d1b4d82033765914aa8fc24b58065329b27dc8
Maria <maralcaraz.13@gmail.com>
2018-04-30 18:34:54 -0300

    Actualizacion de Concesionaria Montironi

--- Concesionaria-Montironi-REST/src/ar/edu/ubp/das/src/beans/ClienteBean.java
+++ Concesionaria-Montironi-REST/src/ar/edu/ubp/das/src/beans/ClienteBean.java
@@ -2,15 +2,23 @@ package ar.edu.ubp.das.src.beans;
 
 import ar.edu.ubp.das.db.Bean;
 
-public class EstadoCuentaBean implements Bean{
+public class ClienteBean implements Bean{
 
 	//Atributos
 	private String dniCliente;
 	private String nomCliente;
 	private String emailCliente;
-	
+	private String fechaSorteo;
+	private String idPlan;
+
+	@Override
+	public String toString() {
+		return "ClienteBean [dniCliente=" + dniCliente + ", nomCliente=" + nomCliente + ", emailCliente=" + emailCliente + 
+		", fechaSorteo=" + fechaSorteo + "]";
+	}
+
 	//Constructor publico
-	public EstadoCuentaBean(){}
+	public ClienteBean(){}
 	
 	//Metodos
 	public String getDniCliente() {
@@ -30,5 +38,20 @@ public class EstadoCuentaBean implements Bean{
 	}
 	public void setEmailCliente(String emailCliente) {
 		this.emailCliente = emailCliente;
+	}
+	public String getFechaSorteo() {
+		return fechaSorteo;
+	}
+
+	public void setFechaSorteo(String fechaSorteo) {
+		this.fechaSorteo = fechaSorteo;
+	}
+
+	public String getIdPlan() {
+		return idPlan;
+	}
+
+	public void setIdPlan(String idPlan) {
+		this.idPlan = idPlan;
 	}
 }
