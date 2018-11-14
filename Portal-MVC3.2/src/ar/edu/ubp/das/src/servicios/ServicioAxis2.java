60d2a5ab103e4675bf7b45c39257419bb54f6d60
scenzano <sebastiancenzano@gmail.com>
2018-11-14 00:18:15 -0300

    Cambios para obtener solo texto de return de respuesta AXIS

--- Portal-MVC3.2/src/ar/edu/ubp/das/src/servicios/ServicioAxis2.java
+++ Portal-MVC3.2/src/ar/edu/ubp/das/src/servicios/ServicioAxis2.java
@@ -1,13 +1,20 @@
 package ar.edu.ubp.das.src.servicios;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.MimeHeaders;
+import javax.xml.soap.SOAPBody;
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
 
 import org.apache.http.NameValuePair;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import ar.edu.ubp.das.src.servicios.axis.utils.DynamicWebServiceClient;
@@ -20,7 +27,7 @@ public class ServicioAxis2 extends ServicioImpl {
 	@Override
 	public String Consumir(String operacion, List<NameValuePair> parameters) {
 		
-		String consumo = "";
+		String returnContent = null;
 		String url = this.getUrl()+"?wsdl"; // Esto habria que cambiarlo e incluir el WS aca.
 		
 		DynamicWebServiceClient dynamicClient = new DynamicWebServiceClient();
@@ -44,17 +51,21 @@ public class ServicioAxis2 extends ServicioImpl {
 		}
 
 		try {
-			consumo = SoapMessageUtil.outputSoapMessage(response);
-			System.out.println("Response: " + consumo);
-			
-			
-			
+	        // get the body
+	        SOAPBody soapBody = response.getSOAPBody();
+	        // find your node based on tag name
+	        NodeList nodes = soapBody.getElementsByTagName("ns:return");
+	        // check if the node exists and get the value 
+	        Node node = nodes.item(0);
+	        returnContent = node != null ? node.getTextContent() : "";
+
+	        System.out.println(returnContent);
 		} 
-		catch (SOAPException | IOException e) {
+		catch (SOAPException e) {
 			e.printStackTrace();
 		}
 		
-		return consumo;
+		return returnContent;
 	}
 
 }
